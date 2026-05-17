338b0ed2057eda82e8661e5c10d7f59d743703a2
dom <dominichx001@gmail.com>
2026-05-17 16:29:37 -0400

    energy + pause

--- 1p-orbit-sim/main.cpp
+++ 1p-orbit-sim/main.cpp
@@ -18,7 +18,9 @@ int main(void) {
   float planetRadius = 10;
 
   float starMass = 2000;
-  float planetMass = 100.0f;
+  float planetMass = 400.0f;
+
+  float energy, kinetic, potential;
 
   Vector2 starPosition = {GetScreenWidth()/2.0f, GetScreenHeight()/2.0f};
   Vector2 planetPosition = {starPosition.x - r, starPosition.y};
@@ -34,53 +36,78 @@ int main(void) {
   Vector2 starVelocity = {0.0f, star_vi};
   Vector2 starAcceleration = {0.0f, 0.0f};
 
+  Vector2 barycenter;
+
+  barycenter.x = (starMass * starPosition.x + planetMass * planetPosition.x) / (planetMass + starMass);
+  barycenter.y = (starMass * starPosition.y + planetMass * planetPosition.y) / (planetMass + starMass);
+
   std::vector<Vector2> planetTrail;
   std::vector<Vector2> starTrail;
   int maxTrailLen = 1250;
   int maxStarTrailLen = 1500;
 
+  bool pause = 0;
+  int framecounter = 0;
+
   SetTargetFPS(60);
 
   while (!WindowShouldClose()) {
-    float dy = starPosition.y - planetPosition.y;
-    float dx = starPosition.x - planetPosition.x;
 
-    float dstSq = dy * dy + dx * dx;
-    float dst = std::sqrt(dstSq);
+    if(IsKeyPressed(KEY_SPACE)) pause = !pause;
 
-    float xdir = dx / dst;
-    float ydir = dy / dst;
+    if(!pause) {
+      float pv_mag = std::sqrt(pow(planetVelocity.x, 2) + pow(planetVelocity.y, 2));
+      float sv_mag = std::sqrt(pow(starVelocity.x, 2) + pow(starVelocity.y, 2));
+      float p_KE = 0.5f * planetMass * pow(pv_mag, 2);
+      float s_KE = 0.5f * starMass * pow(sv_mag, 2);
 
-    float p_accel = G * starMass / dstSq;
-    float s_accel = G * planetMass / dstSq;
-    
-    planetAcceleration.x = xdir * p_accel;
-    planetAcceleration.y = ydir * p_accel;
+      kinetic = p_KE + s_KE;
 
-    starAcceleration.x = -1 * xdir * s_accel;
-    starAcceleration.y = -1 * ydir * s_accel;
+      float dy = starPosition.y - planetPosition.y;
+      float dx = starPosition.x - planetPosition.x;
 
-    planetVelocity.x += planetAcceleration.x;
-    planetVelocity.y += planetAcceleration.y;
+      float dstSq = dy * dy + dx * dx;
+      float dst = std::sqrt(dstSq);
 
-    starVelocity.x += starAcceleration.x;
-    starVelocity.y += starAcceleration.y;
+      potential = -1 * G * planetMass * starMass / dst;
+      energy = potential + kinetic;
 
-    planetPosition.x += planetVelocity.x;
-    planetPosition.y += planetVelocity.y;
+      float xdir = dx / dst;
+      float ydir = dy / dst;
 
-    starPosition.x += starVelocity.x;
-    starPosition.y += starVelocity.y;
+      float p_accel = G * starMass / dstSq;
+      float s_accel = G * planetMass / dstSq;
+      
+      planetAcceleration.x = xdir * p_accel;
+      planetAcceleration.y = ydir * p_accel;
 
-    planetTrail.push_back(planetPosition);
-    starTrail.push_back(starPosition);
+      starAcceleration.x = -1 * xdir * s_accel;
+      starAcceleration.y = -1 * ydir * s_accel;
 
-    if (planetTrail.size() > maxTrailLen) {
-      planetTrail.erase(planetTrail.begin());
-    }
+      planetVelocity.x += planetAcceleration.x;
+      planetVelocity.y += planetAcceleration.y;
 
-    if (starTrail.size() > maxStarTrailLen) {
-      starTrail.erase(starTrail.begin());
+      starVelocity.x += starAcceleration.x;
+      starVelocity.y += starAcceleration.y;
+
+      planetPosition.x += planetVelocity.x;
+      planetPosition.y += planetVelocity.y;
+
+      starPosition.x += starVelocity.x;
+      starPosition.y += starVelocity.y;
+
+      planetTrail.push_back(planetPosition);
+      starTrail.push_back(starPosition);
+
+      if (planetTrail.size() > maxTrailLen) {
+        planetTrail.erase(planetTrail.begin());
+      }
+
+      if (starTrail.size() > maxStarTrailLen) {
+        starTrail.erase(starTrail.begin());
+      }
+    } else {
+      framecounter++;
     }
 
     BeginDrawing();
@@ -108,12 +135,21 @@ int main(void) {
           DrawLineEx(starTrail[i - 1], starTrail[i], 2.0f, trailColor);
       }
 
+      // DrawCircleV(barycenter, 1.0f, WHITE);
+      
       DrawCircleV(starPosition, starRadius * 1.5f, Color{255, 220, 80, 80});
       DrawCircleV(starPosition, starRadius, YELLOW);
 
       DrawCircleV(planetPosition, planetRadius * 2.0f, Color{50, 100, 255, 40});
       DrawCircleV(planetPosition, planetRadius, BLUE);
       DrawCircleV({planetPosition.x - 3, planetPosition.y - 3}, planetRadius * 0.35f, Color{120, 180, 255, 180});
+
+      DrawText(TextFormat("Energy: %.0f J", energy), 10, 30, 20, WHITE);
+
+      DrawText("Press SPACE to pause simulation", 10, GetScreenHeight() - 25, 20, RAYWHITE);
+
+      int textWidth = MeasureText("PAUSED", 30);
+      if(pause && ((framecounter/30)%2)) DrawText("PAUSED", GetScreenWidth()/2 - textWidth/2, GetScreenHeight()/2 - 15, 30, GRAY);
 
       DrawFPS(10, 10);
 
